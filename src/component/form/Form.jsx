ef740a6a3fa22c05af77e8d25d332b6cea067d40
CanseFr <jucansell@gmail.com>
2023-11-04 16:52:35 +0100

    🐛 Finalise auto geolocalisation on click

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -1,8 +1,11 @@
 import styles from "./Form.module.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Button from "../button/Button.jsx";
 import {useNavigate} from "react-router-dom";
 import BackButton from "../back-button/BackButton.jsx";
+import {useUrlPosition} from "../../hooks/useUrlPosition.js";
+import Message from "../message/Message.jsx";
+import Spinner from "../spinner/Spinner.jsx";
 
 
 export function convertToEmoji(countryCode) {
@@ -13,14 +16,45 @@ export function convertToEmoji(countryCode) {
     return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = 'http://api.bigdatacloud.net/data/reverse-geocode-client'
+
 export default function Form() {
 
     const navigate = useNavigate()
-
+    const [lat,lng] = useUrlPosition()
     const [cityName, setCityName] = useState("");
     const [country, setCountry] = useState("");
     const [date, setDate] = useState(new Date());
     const [notes, setNotes] = useState("");
+    const [emoji, setEmoji] = useState()
+    const [isLoadingGeocoding, setIsloadingGeocoding] = useState(false)
+    const [geocodingError, setGeocodingError] = useState('')
+
+    useEffect(() => {
+        async function fetchCityData(){
+            try{
+                setIsloadingGeocoding(true)
+                setGeocodingError("")
+                const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`)
+                const data = await res.json()
+                console.log(data)
+
+                if (!data.countryCode) throw new Error('That doesn\'t seem to be a city. Click somewhere else')
+
+                setCityName(data.city || data.locality || "")
+                setCountry(data.countryName)
+                setEmoji(convertToEmoji(data.countryCode))
+            }catch (err){
+                setGeocodingError(err.message)
+            }finally {
+                setIsloadingGeocoding(false)
+            }
+        }
+        fetchCityData()
+    }, [lat, lng]);
+
+    if (isLoadingGeocoding) return <Spinner />
+    if (geocodingError) return <Message message={geocodingError} />
 
     return (
         <form className={styles.form}>
@@ -31,7 +65,7 @@ export default function Form() {
                     onChange={(e) => setCityName(e.target.value)}
                     value={cityName}
                 />
-                {/* <span className={styles.flag}>{emoji}</span> */}
+                 <span className={styles.flag}>{emoji}</span>
             </div>
 
             <div className={styles.row}>
--- src/component/map/Map.jsx
+++ src/component/map/Map.jsx
@@ -1,20 +1,69 @@
 import style from "./Map.module.css"
 import {useNavigate, useSearchParams} from "react-router-dom";
+import {MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents} from "react-leaflet";
+import {useEffect, useState} from "react";
+import {useCities} from "../../contexts/CitiesContext.jsx";
+import Button from "../button/Button.jsx";
+import {useGeolocation} from "../../hooks/useGeolocation.js";
+import {useUrlPosition} from "../../hooks/useUrlPosition.js";
 
 export default function Map() {
 
+    const {cities} = useCities()
+
+    const [mapPosition, setMapPosition] = useState([100, 0])
+    const {isLoading: isLoadingPostion, position: geolocationPosition, getPosition} = useGeolocation();
+
+    // const [searchParams] = useSearchParams();
+    // const lat = searchParams.get("lat")
+    // const lng = searchParams.get("lng")
+
+    const [lat,lng] = useUrlPosition()
+
+    useEffect(() => {
+        if (lat && lng) setMapPosition([lat,lng])
+    }, [lat, lng]);
+
+    useEffect(() => {
+        if(geolocationPosition)setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    }, [geolocationPosition]);
+
+    return (
+        <div className={style.mapContainer}>
+            {!geolocationPosition && (<Button type='position' onClick={getPosition}>
+                {isLoadingPostion ? 'Loading...' : "Use your position"}
+            </Button>)}
+            <MapContainer className={style.map} center={mapPosition} zoom={13} scrollWheelZoom={true} >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {cities.map(city =>
+                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                        <Popup>
+                            <span>{city.emoji}</span>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                )}
+                <ChangerCenter position={mapPosition}/>
+                <DetectClick/>
+            </MapContainer>
+        </div>
+    );
+}
+
+function ChangerCenter({position}){
+    const map = useMap()
+    map.setView(position, 9)
+    return null
+}
+
+function DetectClick(){
     const navigate = useNavigate()
+    useMapEvents({
+        click: (e) => navigate((`form?lat=${e.latlng.lat}&${e.latlng.lng}`)),
+    })
 
-    const [searchParams, setSearchParams] = useSearchParams();
-
-    const lat = searchParams.get("lat")
-    const lng = searchParams.get("lng")
-
-  return (
-   <div className={style.mapContainer} onClick={()=>{navigate("form")}}>
-     <h1>Map</h1>
-       <h1>Position: {lat}, {lng}</h1>
-   </div>
-  );
 }
 
